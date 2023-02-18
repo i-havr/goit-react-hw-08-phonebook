cf70c06119b32c428fdabc03e22113247197f99e
i-havr <i.havrylov@gmail.com>
2023-02-18 14:46:43 +0200

    add fixes

--- src/components/ContactForm.jsx/ContactForm.jsx
+++ src/components/ContactForm.jsx/ContactForm.jsx
@@ -61,7 +61,6 @@ export default function ContactForm() {
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
           required
-          autoFocus
         />
       </LabelInputStyled>
 
